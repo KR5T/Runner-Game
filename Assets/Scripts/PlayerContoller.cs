3fedbf232126f471e0efbc9537bb4f1af1b4ead7
KR5T <sakarya1869@gmail.com>
2025-09-10 03:04:28 +0300

    rastrain player using clamp

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -8,6 +8,8 @@ public class PlayerContoller : MonoBehaviour
     Rigidbody rigby;
     Vector2 movement;
     public float moveInputSpeed = 5f;
+    public float xClamp = 4.21f;
+    public float zClamp = 3f;
 
     void Start()
     {
@@ -29,12 +31,12 @@ public class PlayerContoller : MonoBehaviour
     private void HandleMovement()
     {
         Vector3 currentPosition = rigby.position;
-        Vector3 moveDirection = new Vector3(movement.x, 0f, 0f);
+        Vector3 moveDirection = new Vector3(movement.x, 0f, movement.y);
         Vector3 newPosition = currentPosition + moveDirection * moveInputSpeed * Time.deltaTime;
+        newPosition.x = Mathf.Clamp(newPosition.x, -xClamp, xClamp);
+        newPosition.z = Mathf.Clamp(newPosition.z, -zClamp, zClamp);
         //UnityDocs: Rigidbody.MovePosition
         rigby.MovePosition(newPosition);
+        
     }
-
-    
-
 }
