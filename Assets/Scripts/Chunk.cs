88d051d97ae0f32da9c37b3ab80a3f161a480d01
KR5T <sakarya1869@gmail.com>
2025-09-27 18:38:31 +0300

    Lane Detection

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -14,8 +14,20 @@ public class Chunk : MonoBehaviour
 
     void SpawnFence()
     {
-        int laneIndex = Random.Range(0, lane.Length);
-        Vector3 spawnPosition = new Vector3(lane[laneIndex], transform.position.y, transform.position.z);
-        Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        List<int> availableLane = new List<int> { 1, 2, 3 };
+        int fancesToSpawn = Random.Range(0,3);
+
+        for (int i = 0; i < fancesToSpawn; i++)
+        {
+            int randomLaneIndex = Random.Range(0, availableLane.Count);
+            int selectedIndex = availableLane[randomLaneIndex];
+            availableLane.RemoveAt(randomLaneIndex);
+            
+            
+            Vector3 spawnPosition = new Vector3(lane[randomLaneIndex], transform.position.y, transform.position.z);
+            Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        }
+
+        
     }
 }
